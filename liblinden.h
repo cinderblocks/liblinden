1fbf356433f892879e03e9fa928c5b2a82250e23
Cinder Biscuits <cinder@cinderblocks.biz>
2018-12-22 10:44:24 -0600

    Add dummy liblinden source files to get cmake going

---
+++ liblinden.h
@@ -0,0 +1,31 @@
+/**
+ * @file liblinden.cpp
+ * @brief
+ *
+ * $LicenseInfo:firstyear=2018&license=viewerlgpl$
+ * Second Life Viewer Source Code
+ * Copyright (C) 2018, CinderBlocks Design Co.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation;
+ * version 2.1 of the License only.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Linden Research, Inc., 945 Battery Street, San Francisco, CA  94111  USA
+ * $/LicenseInfo$
+ */
+
+#ifndef LIBLINDEN_H_
+#define LIBLINDEN_H_
+
+#endif // LIBLINDEN_H_
+
